ecfc45b282960ca3850f74ee48ceaba6aacabda1
Roberto Marcal <roberto.marcal@stoneage.com.br>
2021-08-18 00:37:03 -0300

    adding click button test

--- app/src/components/buttons/__tests__/Button.test.tsx
+++ app/src/components/buttons/__tests__/Button.test.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Button, Props } from '../Button';
+import userEvent from '@testing-library/user-event';
 
 describe('Button Test', () => {
 	let props: Props;
 
 	beforeEach(() => {
 		props = {
-			text: 'click here'
+			text: 'click here',
+			onClick: jest.fn(() => null)
 		};
 	});
 	const renderComponent = () => render(<Button {...props} />);
@@ -22,5 +24,15 @@ describe('Button Test', () => {
 		const { getByTestId } = renderComponent();
 
 		expect(getByTestId('ButtonContianer')).toHaveTextContent(props.text);
+	});
+	it('should click its called', () => {
+		const mockClick = jest.fn();
+		props.onClick = mockClick;
+		const { getByTestId } = renderComponent();
+		const button = getByTestId('ButtonContianer');
+
+		userEvent.click(button);
+
+		expect(mockClick).toHaveBeenCalledTimes(1);
 	});
 });
