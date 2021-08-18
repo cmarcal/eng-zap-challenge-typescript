20acaaf4bdce9ad11940aee26c8e806da0a94464
Roberto Marcal <roberto.marcal@stoneage.com.br>
2021-08-17 22:01:59 -0300

    adding eslint rules and clear next home component

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,29 +1,23 @@
 import Document, { DocumentContext } from 'next/document';
+import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
-		const sheet = new ServerStyleSheet();
+		const sheets = new ServerStyleSheet();
 		const originalRenderPage = ctx.renderPage;
 
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
-				});
+		ctx.renderPage = () => {
+			return originalRenderPage({
+				enhanceApp: (App) => (props) => sheets.collectStyles(<App {...props} />)
+			});
+		};
 
-			const initialProps = await Document.getInitialProps(ctx);
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				)
-			};
-		} finally {
-			sheet.seal();
-		}
+		const initialProps = await Document.getInitialProps(ctx);
+
+		return {
+			...initialProps,
+			styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()]
+		};
 	}
 }
