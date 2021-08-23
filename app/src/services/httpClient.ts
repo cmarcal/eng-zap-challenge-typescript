e5586cd6ea427a96241b944c20bfcfb34525cf2f
Roberto Marçal <cmarcal.roberto@gmail.com>
2021-08-23 10:39:05 -0300

    adding credentials in headers

--- app/src/services/httpClient.ts
+++ app/src/services/httpClient.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 export const httpClient = axios.create({
 	headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Access-Control-Allow-Origin': '*', 'Accept': '*/*', 'Access-Control-Allow-Credentials': true },
 	baseURL: process.env.NEXT_PUBLIC_API,
-	timeout: 10000
+	timeout: 10000,
+	withCredentials: true
 });
 
 
