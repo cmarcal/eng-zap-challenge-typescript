91fec2ba3f94dd4d5f723e98431e2058f449079f
Roberto Marçal <cmarcal.roberto@gmail.com>
2021-08-23 09:41:59 -0300

    Adding headers in fetch

--- app/src/services/get.ts
+++ app/src/services/get.ts
@@ -1,1 +1,10 @@
-export const getImmobileList = async() => await fetch(process.env.NEXT_PUBLIC_API as string)
+const headers = new Headers();
+
+const initialGet: any = { 
+  method: 'GET',
+  headers: headers,
+  mode: 'cors',
+  cache: 'default' 
+}
+
+export const getImmobileList = async() => await fetch(process.env.NEXT_PUBLIC_API as string, initialGet)
