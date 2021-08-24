3d87c39b3ffcceb977840b2edfb0c32e792f943c
Roberto Marçal <cmarcal.roberto@gmail.com>
2021-08-24 00:48:47 -0300

    removing values in headers

--- app/src/services/httpClient.ts
+++ app/src/services/httpClient.ts
@@ -4,10 +4,8 @@ export const Api = axios.create({
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
-		'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     Accept: '*/*',
   },
-	withCredentials: false,
   baseURL: process.env.NEXT_PUBLIC_API,
 });
 
