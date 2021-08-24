17a281426a4278a4b6892c64de680e169186bdf0
Roberto Marçal <cmarcal.roberto@gmail.com>
2021-08-23 23:55:32 -0300

    change headers

--- app/src/services/httpClient.ts
+++ app/src/services/httpClient.ts
@@ -4,8 +4,11 @@ export const Api = axios.create({
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Credentials': true,
+		'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     Accept: '*/*',
   },
+	withCredentials: false,
   baseURL: process.env.NEXT_PUBLIC_API,
 });
 
