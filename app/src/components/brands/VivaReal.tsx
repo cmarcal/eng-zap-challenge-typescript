4b1d07041d28f736a38c00a9763c3a79437e4d9c
Roberto Marçal <cmarcal.roberto@gmail.com>
2021-08-24 09:59:39 -0300

    fixings img ulrs to https

--- app/src/components/brands/VivaReal.tsx
+++ app/src/components/brands/VivaReal.tsx
@@ -12,7 +12,7 @@ export const VivaReal = ({width, headerBrand, onClick}: Props) => {
       { headerBrand ?
         <ImageContainer onClick={onClick} width={width || 'auto'} data-testid='VivaRealImage' src="https://inforce.com.br/wp-content/uploads/2016/12/logo-vivareal-azul-claro-min-1.png" alt='Viva Real logo'/>
         : 
-        <ImageContainer onClick={onClick}  width={width || 'auto'} data-testid='VivaRealImage' src="http://cdnfiles.vivareal.com/emails/v2/logo-vivareal.png" alt='Viva Real logo'/>
+        <ImageContainer onClick={onClick}  width={width || 'auto'} data-testid='VivaRealImage' src="https://cdnfiles.vivareal.com/emails/v2/logo-vivareal.png" alt='Viva Real logo'/>
       }
     </>
     )
--- app/src/components/brands/Zap.tsx
+++ app/src/components/brands/Zap.tsx
@@ -4,6 +4,6 @@ import { ImageContainer } from './styles'
 
 export const Zap = ({width, onClick}: BrandsProps) => {
   return (
-    <ImageContainer onClick={onClick} width={width || 'auto'} data-testid='ZapImage' src="http://cdnfiles.vivareal.com/emails/v2/logo_zap.png" alt='Zap logo'/>
+    <ImageContainer onClick={onClick} width={width || 'auto'} data-testid='ZapImage' src="https://cdnfiles.vivareal.com/emails/v2/logo_zap.png" alt='Zap logo'/>
   )
 }
