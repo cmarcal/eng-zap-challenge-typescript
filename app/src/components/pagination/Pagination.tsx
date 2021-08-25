39960704b8fbb04d78514136303e5e5416ab1e71
Roberto Marçal <cmarcal.roberto@gmail.com>
2021-08-25 07:45:40 -0300

    fixing pagination

--- app/src/components/pagination/Pagination.tsx
+++ app/src/components/pagination/Pagination.tsx
@@ -14,9 +14,8 @@ export const Pagination = ({totalItens, path, activePage, changePage}: Props) =>
   const {colorTextByCompanny} = useGetCompannyColor();
 
   const paginationItens = (): Array<ReactElement> => {
-    const totalPage = totalItens / 20;
+    const totalPage = Math.ceil(totalItens / 20);
     const pages = [];
-
     for (let index = 1; index <= totalPage; index++) {
       pages.push(<PaginationItem activeItem={index === activePage} key={index}  onClick={()=> changePage(index)} colorText={colorTextByCompanny(path)}>{index}</PaginationItem>);
     }
